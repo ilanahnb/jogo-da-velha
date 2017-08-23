2e7f09a591026a7662e27b042b3dcce5a3f08f59
Ilana Boeira <ilanaboeira@gmail.com>
2017-08-23 19:24:19 +1000

    added presentational components

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import RaisedButton from 'material-ui/RaisedButton'
+import NavDrawer from '../components/NavDrawer'
+import { Header, Main } from '../styled/Template'
+
 
 injectTapEventPlugin()
 
@@ -10,17 +12,13 @@ class Template extends Component {
     return (
       <MuiThemeProvider>
         <div>
-          <header>
-            <h1>Jogo da Velha</h1>
-            <RaisedButton
-              label={'Test Button'}
-              primary={true}
-              onTouchTap={()=>console.log('testing button')}
-            />
-          </header>
-          <main>
+          <NavDrawer/>
+          <Header>
+            Jogo da Velha
+          </Header>
+          <Main>
             {this.props.children}
-          </main>
+          </Main>
         </div>
       </MuiThemeProvider>
     )
